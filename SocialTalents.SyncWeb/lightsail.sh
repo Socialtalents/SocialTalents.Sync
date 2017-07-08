36c7fa3ff5d8f25a7e95648cc12ddda55a68aebb
fiji <fiji@socialtalents.com>
2017-07-08 09:40:46 -0700

    shell script to run app on Ubuntu 16.04

---
+++ SocialTalents.SyncWeb/lightsail.sh
@@ -0,0 +1,20 @@
+﻿# Shell script to launch Sync server on Unbuntu 16.04 (tested on amazon lightsail)
+
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list' 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893 
+sudo apt-get update 
+
+yes Y | sudo apt-get install dotnet-dev-1.0.4 
+
+# Cannot use ~ (home) under sudo
+cd /home/ubuntu
+
+# Downloading sources from git, need to create release for tag Stable
+wget https://github.com/Socialtalents/SocialTalents.Sync/archive/Stable.tar.gz 
+tar -xzvf Stable.tar.gz 
+
+chown -R ubuntu: SocialTalents.Sync-Stable
+chmod -R u+w SocialTalents.Sync-Stable
+
+runuser -l ubuntu -c 'cd SocialTalents.Sync-Stable/SocialTalents.SyncWeb && dotnet restore'
+runuser -l ubuntu -c 'cd SocialTalents.Sync-Stable/SocialTalents.SyncWeb && sudo dotnet run'
